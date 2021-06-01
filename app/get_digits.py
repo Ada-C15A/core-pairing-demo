9707fe7753f11eed0182589dbb67093c77a5952c
Trenisha Goslee <trenisha.goslee@ip-172-30-11-176.us-east-2.compute.internal>
2021-06-01 18:02:34 -0400

    update to get_digits

--- app/get_digits.py
+++ app/get_digits.py
@@ -1,2 +1,9 @@
-def get_digits():
-    pass
+def get_digits(characters):
+    if not characters:
+        return None
+
+    digits = []
+    for character in characters:
+        if character.isdigit():
+            digits.append(character)
+    return int("".join(digits))
--- tests/test_digits.py
+++ tests/test_digits.py
@@ -4,3 +4,8 @@ def test_get_digits():
     input = "0fg3y5!78hw578!"
 
     assert get_digits(input) == 3578578
+
+def test_get_digits_with_empty_string_returns_none():
+    input = ""
+
+    assert get_digits(input) is None
